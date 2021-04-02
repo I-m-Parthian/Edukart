d7c92d39dc9cfc9bc115df4fcd9b5ef3179ed177
Parth Aggarwal <paggarwal127@gmail.com>
2021-04-03 01:19:58 +0530

    create add course page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,11 @@
 class Course < ApplicationRecord
 
+    validates :course_name, :description, presence: true
+    
     has_one_attached :image_name
+    validate :check_file_presence
+
+    def check_file_presence
+        errors.add(:image_name, :blank) unless image_name.attached?
+    end
 end
